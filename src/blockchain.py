ca31056fabca42a5719c5de69d222befbd6d8b16
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2018-04-21 08:35:55 +0545

    Difficulty Level Added

--- src/blockchain.py
+++ src/blockchain.py
@@ -2,16 +2,27 @@ import hashlib
 import datetime
 class Block:
     def __init__(self,index, data, previousHash):
+        self.sugar = 0
+        self.level = 8
         self.index = index
         self.timestamp = datetime.datetime.now().timestamp()
         self.previousHash = previousHash
         self.data = data
-        self.hash = self.calculateHash()
-
+        self.hash = self.mine()
+        
+    
     def calculateHash(self):
         myHash = hashlib.sha256()
-        myHash.update((str(self.index) + str(self.timestamp)+str(self.previousHash) + str(self.data)).encode())
+        myHash.update((str(self.index) + str(self.timestamp)+str(self.previousHash) + str(self.data)+ str(self.sugar)).encode())
         return(myHash.hexdigest())
+
+    def mine(self):
+        minedHash =''
+        print("Mining for Block %s......"%self.index)
+        while(minedHash[0:self.level] != "0" * self.level):
+            minedHash =self.calculateHash()
+            self.sugar +=1
+        return minedHash
 
 class BlockChain:
     def __init__(self):
