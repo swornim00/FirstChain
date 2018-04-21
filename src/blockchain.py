324dca93f9d6e916219b0382498124afc2ffa96c
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2018-04-21 07:43:14 +0545

    Creating Hash Made Easier

--- src/blockchain.py
+++ src/blockchain.py
@@ -6,9 +6,12 @@ class Block:
         self.timestamp = datetime.datetime.now().timestamp()
         self.previousHash = previousHash
         self.data = data
+        self.hash = self.calculateHash()
+
+    def calculateHash(self):
         myHash = hashlib.sha256()
-        myHash.update((str(self.index) + str(self.timestamp)+str(previousHash) + str(data)).encode())
-        self.hash = myHash.hexdigest()
+        myHash.update((str(self.index) + str(self.timestamp)+str(self.previousHash) + str(self.data)).encode())
+        return(myHash.hexdigest())
 
 class BlockChain:
     def __init__(self):
